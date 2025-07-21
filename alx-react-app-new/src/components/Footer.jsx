cb4eb9d3ae528d1e6d20db39b63b171c9c5c3370
Amine4jh <amine4jh@gmail.com>
2025-07-21 15:26:37 +0100

    Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer>
-            <p>© 2023 City Lovers</p>
+            <p style={{ color: 'green', fontSize: '10px' }}>© 2023 City Lovers</p>
         </footer>
     )
 }
