f2d1797ffc10981c8875a7f0a98d3babcb190d1b
Amine4jh <amine4jh@gmail.com>
2025-07-22 17:42:26 +0100

    Fix UserProfile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
 function UserProfile(props) {
+    const userData = useContext(UserContext)
+
     return (
         <div>
-            <h2>{props.name}</h2>
+            <h2>{userData.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
         </div>
