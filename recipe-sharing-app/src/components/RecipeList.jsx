610188d0553ee30d8e454526afd628ccd575d2f8
Amine4jh <amine4jh@gmail.com>
2025-07-29 23:02:22 +0100

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,10 +1,12 @@
 import useRecipeStore from "./recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
 	const recipes = useRecipeStore(state => state.recipes)
 
   return (
     <div>
+			<Link to={`/`}>Home</Link>
 			{recipes.map(recipe => (
 				<div key={recipe.id}>
 					<h3>{recipe.title}</h3>
