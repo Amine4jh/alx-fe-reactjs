823e48d3fbb478a0fdafa580e02c44f7cb523c55
Amine4jh <amine4jh@gmail.com>
2025-07-31 20:36:23 +0100

    Change profile component with Home Component (Setting up)

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<h1>Welcome Home!</h1>}/>
-        {/* <Route path='/about' element={<h1>About</h1>}/> */}
+        <Route path='/' element={<Home />}/>
+        {/* <Route path='/' element={}/> */}
       </Routes>
     </Router>
   )
