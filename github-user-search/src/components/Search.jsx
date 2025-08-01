9c9e28b3e749a000b50e9e56c88f09b8ae0ae73a
Amine4jh <amine4jh@gmail.com>
2025-08-01 16:24:29 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -3,19 +3,45 @@ import { fetchUserData } from "../services/githubService"
 
 const Search = () => {
   const [ username, setUsername ] = useState('')
-  const [ user, setUser ] = useState(null)
+  const [ location, setLocatiion ] = useState('')
+  const [ minRepos, setMinRepos ] = useState('')
+  const [ results, setResults ] = useState([])
+  // const [ user, setUser ] = useState(null)
   const [ loading, setLoading ] = useState(false)
   const [ error, setError ] = useState(false)
+  const [ page, setPage ] = useState(1)
+  const [ totalCount, setTotalCount ] = useState(0)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     setLoading(true)
     setError(false)
-    setUser(null)
+    // setUser(null)
+    setResults([])
+    setPage(1)
 
     try {
-      const data = await fetchUserData(username)
-      setUser(data)
+      const data = await fetchUserData(username, location, minRepos, 1)
+      if (data.total_count === 0) {
+        setError(true)
+      } else {
+        setResults(data.items)
+        setTotalCount(data.total_count)
+      }
+    } catch {
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleLoadMore = async () => {
+    const nextPage = page + 1
+    setPage(nextPage)
+    setLoading(true)
+    try {
+      const data = await fetchUserData(username, location, minRepos, nextPage)
+      setResults(prev => [...prev, ...data.items])
     } catch {
       setError(true)
     } finally {
@@ -32,24 +58,45 @@ const Search = () => {
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <input
+        type="text" 
+        placeholder="Location..."
+        value={location}
+        onChange={(e) => setLocatiion(e.target.value)}
+      />
+      <input
+        type="number" 
+        placeholder="Min Repos..."
+        value={minRepos}
+        onChange={(e) => setMinRepos(e.target.value)}
+      />
       <button type="submit">Search</button>
     </form>
 
     {loading && <p>Loading...</p>}
     {error && <p>Looks like we cant find the user.</p>}
-    {user && (
-      <div style={{ margin: '1rem', padding: '0.5rem', border: '1px solid #333' }}>
-        <img src={user.avatar_url} alt="Avatar" width='150px' style={{ borderRadius: '50%' }} />
-        <h3><a href={user.html_url} target="_blank">{user.login}</a></h3>
-        <p><strong>Name:</strong> {user.name}</p>
-        <p><strong>Id:</strong> {user.id}</p>
-        <p><strong>Location:</strong> {user.location}</p>
-        <p><strong>Followers:</strong> {user.followers}</p>
-        <p><strong>Following:</strong> {user.following}</p>
-        <p><strong>Public Repositories:</strong> {user.public_repos}</p>
-        {user.bio && <p><strong>Bio:</strong> {user.bio}</p>}
-      </div>
+    {results.length > 0 && (
+      <ul>
+        <p>Results: {results.length}</p>
+        {results.map(user => (
+          <li style={{ border: "1px solid #333", listStyle: "none", margin: "10px", padding: "10px"}}>
+            <h3>{user.login}</h3>
+            {location && <p><b>Location:</b> {location}</p>}
+            <a href={user.html_url} target="_blank">View Profile</a>
+          </li>
+        ))}
+      </ul>
     )}
+
+    {results.length < totalCount && (
+      <button 
+        style={{ margin: "30px" }}
+        onClick={handleLoadMore}
+      >
+        Load More
+      </button>
+    )}
+
   </div>
  ) 
 }
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users/';
+const BASE_URL = 'https://api.github.com/search/users';
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`${BASE_URL}${username}`)
+export const fetchUserData = async (username, location, minRepos, page = 1) => {
+  let query = ""
+
+  if (username) query += `${username}`
+  if (location) query += `+location:${location}`
+  if (minRepos) query += `+repos:>${minRepos}`
+
+  const response = await axios.get(`${BASE_URL}?q=${query}&page=${page}&per_page=10`)
+
   return response.data
 }
